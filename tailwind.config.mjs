c5da51157abba2567ae1091e3a2fe8f3fbf0617b
Paul Scanlon <pauliescanlon@gmail.com>
2024-02-18 12:38:20 -0800

    style: add styles to form

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            label: {
+              fontSize: theme('fontSize.sm'),
+            },
+            input: {
+              border: `1px solid ${theme('colors.zinc.300')}`,
+              borderRadius: theme('borderRadius.DEFAULT'),
+            },
+            button: {
+              padding: `${(theme('spacing.2'), theme('spacing.2'))}`,
+            },
+          },
+        },
+      }),
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
